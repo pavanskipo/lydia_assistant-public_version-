590c14ab873c95b5c7e9169383b25a5b6793cc61
Pavan <pavanskipo@gmail.com>
2018-11-16 22:41:27 +0530

    Initialization file

--- initializeLydia.sh
+++ initializeLydia.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo npm install
+
 echo "export PATH=$PATH:$(pwd)" >> ~/.bashrc
+
 sudo echo "node Lydia/lydia.js" > lydia
 sudo chmod +x lydia
+
 echo "Done..., To call me just type 'lydia' "
 exec bash
